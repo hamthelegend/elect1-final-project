ac1c4113578adfda224f74e2f7bc2c2ddc50517b
hamthelegend <hamuelxd@gmail.com>
2023-01-15 19:56:40 +0800

    Updated ddl.sql
    Added DROP TABLE IF EXISTS for each table.
    Fixed formatting.
    
    Change these field names to prevent conflicts with SQL keywords:
    genre.name -> tag
    copies.medium -> medium_format
    transactions.timestamp -> time_processed

--- ddl.sql
+++ ddl.sql
@@ -14,6 +14,8 @@ CREATE TABLE customers (
     PRIMARY KEY (customer_id)
 );
 
+DROP TABLE IF EXISTS employees;
+
 CREATE TABLE employees (
     employee_id BIGINT,
     last_name VARCHAR(50),
@@ -24,56 +26,58 @@ CREATE TABLE employees (
     PRIMARY KEY (employee_id)
 );
 
+DROP TABLE IF EXISTS genres;
+
 CREATE TABLE genres (
      genre_id BIGINT,
-     name VARCHAR(50),
+     tag VARCHAR(50),
      aisle_number INT
-
 );
+
+DROP TABLE IF EXISTS movies;
 
 CREATE TABLE movies (
     movie_id BIGINT,
-    FK genre_id BIGINT,
+    genre_id BIGINT,
     title VARCHAR(50),
     release_date DATE,
     director VARCHAR(50),
-    PRIMARY KEY(movie_id),
+    PRIMARY KEY (movie_id),
     FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
 );
 
-CREATE TABLE copies(
+DROP TABLE IF EXISTS copies;
+
+CREATE TABLE copies (
     copy_id BIGINT,
     movie_id BIGINT,
-    medium VARCHAR(10),
+    medium_format VARCHAR(10),
     cost DEC(65,2),
     remarks VARCHAR(100),
-    PRIMARY KEY(copy_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(movie_id)
-
+    PRIMARY KEY (copy_id),
+    FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
 );
 
-CREATE TABLE transactions(
+DROP TABLE IF EXISTS transactions;
+
+CREATE TABLE transactions (
     transaction_id BIGINT,
     purchaser_customer_id BIGINT,
     cashier_employee_id BIGINT,
-    timestamp TIMESTAMP,
+    time_processed TIMESTAMP,
     is_return BOOL,
-    PRIMARY KEY(transaction_id),
-    FOREIGN KEY(purchaser_customer_id) REFERENCES customers(customer_id)
+    PRIMARY KEY (transaction_id),
+    FOREIGN KEY (purchaser_customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (cashier_employee_id) REFERENCES employees(employee_id)
 );
-CREATE TABLE transaction_contents(
+
+DROP TABLE IF EXISTS transaction_contents;
+
+CREATE TABLE transaction_contents (
     transaction_content_id BIGINT,
     transaction_id BIGINT,
     copy_id BIGINT,
-    PRIMARY KEY(transaction_content_id),
-    FOREIGN KEY(transaction_id) REFERENCES transactions(transaction_id),
-    FOREIGN KEY(copy_id) REFERENCES copies(copy_id)
+    PRIMARY KEY (transaction_content_id),
+    FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id),
+    FOREIGN KEY (copy_id) REFERENCES copies(copy_id)
 );
-
-# Insert remaining table in the following order:
-# employees
-# genres
-# movies
-# copies
-# transactions
-# transaction_contents
