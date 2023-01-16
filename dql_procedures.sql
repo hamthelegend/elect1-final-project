a56bb76fbc7a4aee1c3618e24397b1bbb03df8ec
hamthelegend <hamuelxd@gmail.com>
2023-01-16 15:42:48 +0800

    Added dql_procedures.sql

---
+++ dql_procedures.sql
@@ -0,0 +1,221 @@
+USE video_town;
+
+CREATE PROCEDURE customers()
+BEGIN
+    SELECT * FROM customers;
+END;
+
+CREATE PROCEDURE customer(_customer_id BIGINT)
+BEGIN
+    SELECT * FROM customers WHERE customer_id = _customer_id;
+END;
+
+CREATE PROCEDURE employees()
+BEGIN
+    SELECT * FROM employees;
+END;
+
+CREATE PROCEDURE employee(_employee_id BIGINT)
+BEGIN
+    SELECT * FROM employees WHERE employee_id = _employee_id;
+END;
+
+CREATE PROCEDURE genres()
+BEGIN
+    SELECT * FROM genres;
+END;
+
+CREATE PROCEDURE genre(_genre_id BIGINT)
+BEGIN
+    SELECT * FROM genres WHERE genre_id = _genre_id;
+END;
+
+CREATE PROCEDURE movies()
+BEGIN
+    SELECT movie_id,
+           title,
+           release_date,
+           director,
+           g.genre_id,
+           tag AS genre,
+           aisle_number
+    FROM movies m
+             JOIN genres g ON g.genre_id = m.genre_id;
+END;
+
+CREATE PROCEDURE movie(_movie_id BIGINT)
+BEGIN
+    SELECT movie_id,
+           title,
+           release_date,
+           director,
+           g.genre_id,
+           tag AS genre,
+           aisle_number
+    FROM movies m
+             JOIN genres g ON g.genre_id = m.genre_id
+    WHERE movie_id = _movie_id;
+END;
+
+CREATE PROCEDURE copies()
+BEGIN
+    SELECT copy_id,
+           medium_format,
+           cost,
+           remarks,
+           m.movie_id,
+           title,
+           release_date,
+           director,
+           g.genre_id,
+           tag AS genre,
+           aisle_number
+    FROM copies c
+             JOIN movies m ON m.movie_id = c.movie_id
+             JOIN genres g ON g.genre_id = m.genre_id;
+END;
+
+CREATE PROCEDURE copy(_copy_id BIGINT)
+BEGIN
+    SELECT copy_id,
+           medium_format,
+           cost,
+           remarks,
+           m.movie_id,
+           title,
+           release_date,
+           director,
+           g.genre_id,
+           tag AS genre,
+           aisle_number
+    FROM copies c
+             JOIN movies m ON m.movie_id = c.movie_id
+             JOIN genres g ON g.genre_id = m.genre_id
+    WHERE copy_id = _copy_id;
+END;
+
+CREATE PROCEDURE transactions()
+BEGIN
+    SELECT t.transaction_id,
+           c.customer_id                          AS customer_id,
+           CONCAT(c.first_name, ' ', c.last_name) AS customer,
+           e.employee_id                          AS cashier_employee_id,
+           CONCAT(e.first_name, ' ', e.last_name) AS cashier_employee,
+           time_processed,
+           is_return,
+           GROUP_CONCAT(title)                    AS items,
+           is_closed
+    FROM transactions t
+             JOIN customers c ON c.customer_id = t.customer_id
+             JOIN employees e ON e.employee_id = t.cashier_employee_id
+             JOIN transaction_contents tc ON t.transaction_id = tc.transaction_id
+             JOIN copies cp ON tc.copy_id = cp.copy_id
+             JOIN movies m ON cp.movie_id = m.movie_id
+    GROUP BY transaction_id;
+END;
+
+CREATE PROCEDURE rent_transactions()
+BEGIN
+    SELECT t.transaction_id,
+           c.customer_id                          AS customer_id,
+           CONCAT(c.first_name, ' ', c.last_name) AS customer,
+           e.employee_id                          AS cashier_employee_id,
+           CONCAT(e.first_name, ' ', e.last_name) AS cashier_employee,
+           time_processed,
+           is_return,
+           GROUP_CONCAT(title)                    AS items,
+           SUM(cp.cost)                           AS totalCost,
+           is_closed
+    FROM transactions t
+             JOIN customers c ON c.customer_id = t.customer_id
+             JOIN employees e ON e.employee_id = t.cashier_employee_id
+             JOIN transaction_contents tc ON t.transaction_id = tc.transaction_id
+             JOIN copies cp ON tc.copy_id = cp.copy_id
+             JOIN movies m ON cp.movie_id = m.movie_id
+    WHERE NOT is_return
+    GROUP BY transaction_id;
+END;
+
+CREATE PROCEDURE return_transactions()
+BEGIN
+    SELECT t.transaction_id,
+           c.customer_id                          AS customer_id,
+           CONCAT(c.first_name, ' ', c.last_name) AS customer,
+           e.employee_id                          AS cashier_employee_id,
+           CONCAT(e.first_name, ' ', e.last_name) AS cashier_employee,
+           time_processed,
+           is_return,
+           GROUP_CONCAT(title)                    AS items,
+           is_closed
+    FROM transactions t
+             JOIN customers c ON c.customer_id = t.customer_id
+             JOIN employees e ON e.employee_id = t.cashier_employee_id
+             JOIN transaction_contents tc ON t.transaction_id = tc.transaction_id
+             JOIN copies cp ON tc.copy_id = cp.copy_id
+             JOIN movies m ON cp.movie_id = m.movie_id
+    WHERE is_return
+    GROUP BY transaction_id;
+END;
+
+CREATE PROCEDURE transaction(_transaction_id BIGINT)
+BEGIN
+    DECLARE is_return BOOL;
+
+    SELECT is_return FROM transactions WHERE transaction_id = _transaction_id;
+
+    IF is_return THEN
+        SELECT t.transaction_id,
+               c.customer_id                          AS customer_id,
+               CONCAT(c.first_name, ' ', c.last_name) AS customer,
+               e.employee_id                          AS cashier_employee_id,
+               CONCAT(e.first_name, ' ', e.last_name) AS cashier_employee,
+               time_processed,
+               is_return,
+               GROUP_CONCAT(title)                    AS items,
+               is_closed
+        FROM transactions t
+                 JOIN customers c ON c.customer_id = t.customer_id
+                 JOIN employees e ON e.employee_id = t.cashier_employee_id
+                 JOIN transaction_contents tc ON t.transaction_id = tc.transaction_id
+                 JOIN copies cp ON tc.copy_id = cp.copy_id
+                 JOIN movies m ON cp.movie_id = m.movie_id
+        WHERE t.transaction_id = _transaction_id
+        GROUP BY transaction_id;
+    ELSE
+        SELECT t.transaction_id,
+               c.customer_id                          AS customer_id,
+               CONCAT(c.first_name, ' ', c.last_name) AS customer,
+               e.employee_id                          AS cashier_employee_id,
+               CONCAT(e.first_name, ' ', e.last_name) AS cashier_employee,
+               time_processed,
+               is_return,
+               GROUP_CONCAT(title)                    AS items,
+               SUM(cp.cost)                           AS totalCost,
+               is_closed
+        FROM transactions t
+                 JOIN customers c ON c.customer_id = t.customer_id
+                 JOIN employees e ON e.employee_id = t.cashier_employee_id
+                 JOIN transaction_contents tc ON t.transaction_id = tc.transaction_id
+                 JOIN copies cp ON tc.copy_id = cp.copy_id
+                 JOIN movies m ON cp.movie_id = m.movie_id
+        WHERE t.transaction_id = _transaction_id
+        GROUP BY transaction_id;
+    END IF;
+END;
+
+CREATE PROCEDURE transaction_contents(_transaction_id BIGINT)
+BEGIN
+    SELECT
+        transaction_id,
+        transaction_content_id,
+        c.copy_id,
+        medium_format,
+        cost,
+        m.movie_id,
+        title AS movie,
+        release_date
+    FROM transaction_contents tc
+    JOIN copies c ON c.copy_id = tc.copy_id
+    JOIN movies m ON m.movie_id = c.movie_id
+    WHERE transaction_id = _transaction_id;
+END;
