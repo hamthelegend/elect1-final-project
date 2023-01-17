9d32a4db0bf4edd3ba38b2f607c9816ccff51905
David <89499423+Cov3n4nt@users.noreply.github.com>
2023-01-17 00:15:36 -0800

    Update playground.sql

--- playground.sql
+++ playground.sql
@@ -32,7 +32,7 @@ CALL transaction_contents(8);
 
 CALL copies_rented_by_month(2021);
 
-CALL top_five_movies_from_genre_by_revenue(3);
+CALL top_five_movies_from_genre_by_revenue(5);
 
 CALL top_ten_movies_by_revenue();
 
@@ -45,5 +45,7 @@ CALL best_employee_by_year;
 CALL unreturned_copies;
 
 CALL start_seed_transaction(1, 1, '2023-01-17 15:10:59.934792', TRUE);
+
 CALL add_seed_item(1);
+
 CALL end_seed_transaction();
